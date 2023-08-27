d7b1804006b583bc9f5d6e42f46cb40a513fd2a8
eyalBPM <eyal@bpm.org.il>
2023-08-27 11:15:56 +0300

    complete

--- pyBpmExternal.tab/Openings.panel/OpeningSetter.pushbutton/script.py
+++ pyBpmExternal.tab/Openings.panel/OpeningSetter.pushbutton/script.py
@@ -2,7 +2,7 @@
 """ This script iterates over all the openings (Generic Model from the BPM library) and dose the following:
 - Copies the Elevation to a taggable parameter (useful in versions 20+21).
 - Copies the Reference Level to a taggable parameter.
-- Sets Mark to opening it is missing.
+- Sets Mark to opening if it is missing.
 - Defines whether the opening is located in the floor or not.
 - Calculates the projected height of the opening.
 - Calculates the absolute height of the opening. """
@@ -22,24 +22,13 @@ clr.AddReferenceByPartialName('System.Windows.Forms')
 from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, Transaction, BuiltInParameter
 from Autodesk.Revit.UI import TaskDialog
 
-# from System.Collections.Generic import List
-
-# import Autodesk.Windows as aw
-
 max_elements = 5
 gdict = globals()
 uiapp = __revit__
 uidoc = uiapp.ActiveUIDocument
 if uidoc:
     doc = uiapp.ActiveUIDocument.Document
-    # selection = [doc.GetElement(x) for x in uidoc.Selection.GetElementIds()]
-    # for idx, el in enumerate(selection):
-    #     if idx < max_elements:
-    #         gdict['e{}'.format(idx+1)] = el
-    #     else:
-    #         break
 
-# alert function
 def alert(msg):
     TaskDialog.Show('BPM - Opening Update', msg)
 
@@ -61,29 +50,6 @@ def get_all_openings():
         if gm.Name in opening_names:
             openings.append(gm)
     return openings
-
-# def set_schedule_level(opening):
-#     """ Sets the Schedule Level parameter to the correct level. """
-#     # TODO: Check if is floor, if is floor, get the level of the floor. if it is not floor, get the floor below and set the level to it. if there is no floor below, set the level by the code below.
-#     # ! For now, we decided to not use this function. The user will have to set the level manually. The script will only set the "MEP - Not Required" parameter to true or false by this calculation.
-#     all_levels = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Levels).WhereElementIsNotElementType().ToElements()
-#     all_levels_sorted = sorted(all_levels, key = lambda x: x.Elevation)
-#     all_levels_sorted_length = len(all_levels_sorted)
-#     opening_location_point_z = opening.Location.Point.Z
-#     param__schedule_level = opening.get_Parameter(BuiltInParameter.INSTANCE_SCHEDULE_ONLY_LEVEL_PARAM)
-#     if not param__schedule_level:
-#         return
-#     if all_levels_sorted[0].Elevation >= opening_location_point_z:
-#         param__schedule_level.Set(all_levels_sorted[0].Id)
-#         return
-#     if all_levels_sorted[all_levels_sorted_length - 1].Elevation <= opening_location_point_z:
-#         param__schedule_level.Set(all_levels_sorted[all_levels_sorted_length - 1].Id)
-#         return
-#     for i in range(all_levels_sorted_length - 1):
-#         if all_levels_sorted[i].Elevation <= opening_location_point_z and all_levels_sorted[i + 1].Elevation > opening_location_point_z:
-#             param__schedule_level.Set(all_levels_sorted[i].Id)
-#             return
-#     print('No level found for opening: {}'.format(opening.Id))
 
 def is_floor(opening):
     """ Returns True if the host of the opening is a floor, else returns False.
