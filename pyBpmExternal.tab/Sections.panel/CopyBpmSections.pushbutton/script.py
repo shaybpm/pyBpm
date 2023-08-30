8da17072326bdc4bff8f21f829669bfdcfa438df
eyalBPM <eyal@bpm.org.il>
2023-08-30 13:38:45 +0300

    Work with ElementTransformUtils.CopyElements

--- pyBpmExternal.tab/Sections.panel/CopyBpmSections.pushbutton/script.py
+++ pyBpmExternal.tab/Sections.panel/CopyBpmSections.pushbutton/script.py
@@ -13,7 +13,10 @@ clr.AddReferenceByPartialName("PresentationFramework")
 clr.AddReferenceByPartialName('System')
 clr.AddReferenceByPartialName('System.Windows.Forms')
 
-from Autodesk.Revit.DB import Transaction, FilteredElementCollector, RevitLinkInstance, ViewType
+from System.Collections.Generic import List
+
+from Autodesk.Revit.DB import Transaction, FilteredElementCollector, RevitLinkInstance, ViewType, ElementTransformUtils, ElementId, BuiltInParameter, CopyPasteOptions
+
 from Autodesk.Revit.UI import TaskDialog
 
 from pyrevit import forms
@@ -30,28 +33,45 @@ def alert(msg):
 
 # ------------------------------------------------------------
 
-def get_all_links():
-    return FilteredElementCollector(doc).OfClass(RevitLinkInstance).ToElements()
-
-def get_comp_doc():
-    all_links = get_all_links()
+def get_comp_link():
+    all_links = FilteredElementCollector(doc).OfClass(RevitLinkInstance).ToElements()
     for link in all_links:
         if "COMP" in link.Name:
-              return link.GetLinkDocument()
+              return link
 
 def is_su_sec(view):
-    if not view.ViewType:
-        return False
+	if not view.ViewType:
+		return False
 
-    if not view.ViewType == ViewType.Section:
-        return False
+	if not view.ViewType == ViewType.Section:
+		return False
 
-    if not "SU" in view.Name:
-        return False
+	if not "SU" in view.Name:
+		return False
+	
+	viewport_sheet_number = view.get_Parameter(BuiltInParameter.VIEWPORT_SHEET_NUMBER)
+	if not viewport_sheet_number.AsString():
+		return False
 
-    return True
+	return True
 
 def run():
-    selected_sections = forms.select_views(title='Select Plans', button_name='Copy', width=500, multiple=True, filterfunc=is_su_sec, doc=get_comp_doc())
+    comp_link = get_comp_link()
+    if not comp_link:
+        alert("The Compilation mode link is not loaded.")
+        return
+    comp_doc = comp_link.GetLinkDocument()
+    selected_sections = forms.select_views(title='Select Plans', button_name='Copy', width=500, multiple=True, filterfunc=is_su_sec, doc=comp_doc)
+    if not selected_sections or len(selected_sections) == 0:
+        return
+    
+    opts = CopyPasteOptions()
+    transform = comp_link.GetTotalTransform()
+    ilSection = List[ElementId]([x.Id for x in selected_sections])
+    
+    t = Transaction(doc, 'BPM | Copy Bpm Sections')
+    t.Start()
+    ElementTransformUtils.CopyElements(comp_doc, ilSection, doc, transform, opts)
+    t.Commit()
 
 run()
