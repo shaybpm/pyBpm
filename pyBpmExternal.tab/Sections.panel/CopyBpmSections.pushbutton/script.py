b2bc4acaca220c50a7d5cd078937df246c71779a
eyalBPM <eyal@bpm.org.il>
2023-08-30 12:15:12 +0300

    Copy Bpm Sections

---
+++ pyBpmExternal.tab/Sections.panel/CopyBpmSections.pushbutton/script.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+""" Copy Bpm suggest sections to the current project. """
+__title__ = 'Copy Bpm\nSections'
+__author__ = 'Eyal Sinay'
+
+# ------------------------------------------------------------
