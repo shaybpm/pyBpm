3356fc547c0309b738380b69f30c2457f42678be
eyalBPM <eyal@bpm.org.il>
2023-09-03 09:52:30 +0300

    try is the hook

--- hooks/doc-syncing.py
+++ hooks/doc-syncing.py
@@ -1,24 +1,26 @@
-import clr
-clr.AddReference('RevitAPI')
-
-from Autodesk.Revit.DB import Transaction
-
-# ------------------------------------------------------------
-from pyrevit import EXEC_PARAMS
-doc = EXEC_PARAMS.event_args.Document
-# ------------------------------------------------------------
-
-# Run the "Opening setter" script without printing anything to the console and without showing any dialogs.
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "pyBpmExternal.tab", "Openings.panel", "OpeningSetter.pushbutton", "lib"))
-import OpeningSetter
-
 try:
+	import clr
+	clr.AddReference('RevitAPI')
+
+	from Autodesk.Revit.DB import Transaction
+
+	# ------------------------------------------------------------
+	from pyrevit import EXEC_PARAMS
+	doc = EXEC_PARAMS.event_args.Document
+	# ------------------------------------------------------------
+
+	# Run the "Opening setter" script without printing anything to the console and without showing any dialogs.
+	import os, sys
+	sys.path.append(os.path.join(os.path.dirname(__file__), "..", "pyBpmExternal.tab", "Openings.panel", "OpeningSetter.pushbutton", "lib"))
+	import OpeningSetter
+
 	all_openings = OpeningSetter.get_all_openings(doc)
 	t = Transaction(doc, 'BPM | Opening Update')
 	t.Start()
 	for opening in all_openings:
 		results = OpeningSetter.execute_all_functions(doc, opening, False)
 	t.Commit()
+
 except Exception as e:
-	print("ERROR: {}".format(e))
+	# TODO: If it's development mode, show the error message.
+	pass
