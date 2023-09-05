65e28211226fdf52883e792224668e8bd76e9baf
eyalBPM <eyal@bpm.org.il>
2023-09-05 09:53:24 +0300

    OpeningSetter -> OpeningSet

--- pyBpmExternal.tab/Openings.panel/OpeningSet.pushbutton/script.py
+++ pyBpmExternal.tab/Openings.panel/OpeningSet.pushbutton/script.py
@@ -55,7 +55,7 @@ def print_results(results):
 
 def print_full_results(results):
     output = script.get_output()
-    output.print_html('<h1>Opening Setter</h1>')
+    output.print_html('<h1>Opening Set</h1>')
     output.print_html('<div style="color:gray">Number of openings found: {}</div>'.format(len(results)))
 
     is_any_warning = "WARNING" in [result["status"] for result in results]
