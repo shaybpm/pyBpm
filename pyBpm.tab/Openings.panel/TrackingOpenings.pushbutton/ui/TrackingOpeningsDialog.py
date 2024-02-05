e4b54fe786a5a772ed5005367e91d7435ffd65a9
eyalBPM <eyal@bpm.org.il>
2024-02-05 15:29:47 +0200

    Refactor TrackingOpeningsDialog.py to improve intersection handling

--- pyBpm.tab/Openings.panel/TrackingOpenings.pushbutton/ui/TrackingOpeningsDialog.py
+++ pyBpm.tab/Openings.panel/TrackingOpenings.pushbutton/ui/TrackingOpeningsDialog.py
@@ -21,6 +21,7 @@ from Autodesk.Revit.DB import (
     ViewSheet,
     FilteredElementCollector,
     ElementId,
+    SetComparisonResult,
 )
 
 from System.Collections.Generic import List
@@ -926,9 +927,7 @@ class TrackingOpeningsDialog(Windows.Window):
         level = active_view.GenLevel
         project_elevation = level.Elevation
 
-        t = Transaction(self.doc, "pyBpm | Create Clouds")
-        t.Start()
-
+        curves_tuples = []
         for bbox in bboxes:
             point1 = XYZ(bbox.Min.X, bbox.Min.Y, project_elevation)
             point2 = XYZ(bbox.Min.X, bbox.Max.Y, project_elevation)
@@ -945,8 +944,158 @@ class TrackingOpeningsDialog(Windows.Window):
             curve3 = line3
             curve4 = line4
 
-            i_list_curve = List[Curve]([curve1, curve2, curve3, curve4])
+            max_loops = 100
+            loop_count = 0
+            index_combine = -1
+            while True:
+                is_intersect = False
+                for i, curves_tuple in enumerate(curves_tuples):
+                    for exist_curve in curves_tuple:
+                        for curve in [curve1, curve2, curve3, curve4]:
+                            if (
+                                exist_curve.Intersect(curve)
+                                == SetComparisonResult.Overlap
+                            ):
+                                is_intersect = True
+                                break
+                    if is_intersect:
+                        exist_point_1 = curves_tuple[0].GetEndPoint(0)
+                        exist_point_2 = curves_tuple[1].GetEndPoint(0)
+                        exist_point_3 = curves_tuple[2].GetEndPoint(0)
+                        exist_point_4 = curves_tuple[3].GetEndPoint(0)
+                        old_point1 = point1
+                        old_point2 = point2
+                        old_point3 = point3
+                        old_point4 = point4
 
+                        point1 = XYZ(
+                            min(
+                                exist_point_1.X,
+                                exist_point_2.X,
+                                exist_point_3.X,
+                                exist_point_4.X,
+                                old_point1.X,
+                                old_point2.X,
+                                old_point3.X,
+                                old_point4.X,
+                            ),
+                            min(
+                                exist_point_1.Y,
+                                exist_point_2.Y,
+                                exist_point_3.Y,
+                                exist_point_4.Y,
+                                old_point1.Y,
+                                old_point2.Y,
+                                old_point3.Y,
+                                old_point4.Y,
+                            ),
+                            project_elevation,
+                        )
+                        point2 = XYZ(
+                            min(
+                                exist_point_1.X,
+                                exist_point_2.X,
+                                exist_point_3.X,
+                                exist_point_4.X,
+                                old_point1.X,
+                                old_point2.X,
+                                old_point3.X,
+                                old_point4.X,
+                            ),
+                            max(
+                                exist_point_1.Y,
+                                exist_point_2.Y,
+                                exist_point_3.Y,
+                                exist_point_4.Y,
+                                old_point1.Y,
+                                old_point2.Y,
+                                old_point3.Y,
+                                old_point4.Y,
+                            ),
+                            project_elevation,
+                        )
+                        point3 = XYZ(
+                            max(
+                                exist_point_1.X,
+                                exist_point_2.X,
+                                exist_point_3.X,
+                                exist_point_4.X,
+                                old_point1.X,
+                                old_point2.X,
+                                old_point3.X,
+                                old_point4.X,
+                            ),
+                            max(
+                                exist_point_1.Y,
+                                exist_point_2.Y,
+                                exist_point_3.Y,
+                                exist_point_4.Y,
+                                old_point1.Y,
+                                old_point2.Y,
+                                old_point3.Y,
+                                old_point4.Y,
+                            ),
+                            project_elevation,
+                        )
+                        point4 = XYZ(
+                            max(
+                                exist_point_1.X,
+                                exist_point_2.X,
+                                exist_point_3.X,
+                                exist_point_4.X,
+                                old_point1.X,
+                                old_point2.X,
+                                old_point3.X,
+                                old_point4.X,
+                            ),
+                            min(
+                                exist_point_1.Y,
+                                exist_point_2.Y,
+                                exist_point_3.Y,
+                                exist_point_4.Y,
+                                old_point1.Y,
+                                old_point2.Y,
+                                old_point3.Y,
+                                old_point4.Y,
+                            ),
+                            project_elevation,
+                        )
+
+                        line1 = Line.CreateBound(point1, point2)
+                        line2 = Line.CreateBound(point2, point3)
+                        line3 = Line.CreateBound(point3, point4)
+                        line4 = Line.CreateBound(point4, point1)
+
+                        curve1 = line1
+                        curve2 = line2
+                        curve3 = line3
+                        curve4 = line4
+
+                        index_combine = i
+
+                        break
+                if not is_intersect:
+                    break
+                loop_count += 1
+                if loop_count > max_loops:
+                    break
+
+            if index_combine != -1:
+                curves_tuples[index_combine] = (
+                    curve1,
+                    curve2,
+                    curve3,
+                    curve4,
+                )
+            else:
+                curves_tuples.append((curve1, curve2, curve3, curve4))
+
+        t = Transaction(self.doc, "pyBpm | Create Clouds")
+        t.Start()
+
+        for curves_tuple in curves_tuples:
+            curve1, curve2, curve3, curve4 = curves_tuple
+            i_list_curve = List[Curve]([curve1, curve2, curve3, curve4])
             RevisionCloud.Create(self.doc, active_view, revision.Id, i_list_curve)
 
         t.Commit()
