2e876a12b11c6b7fd9833625a00c318a1ab07315
eyalBPM <eyal@bpm.org.il>
2024-02-26 10:41:27 +0200

    Add HtmlUtils module and refactor workset renaming logic

--- pyBpm.tab/BIM.panel/CreateWorksets.pushbutton/lib/create_worksets.py
+++ pyBpm.tab/BIM.panel/CreateWorksets.pushbutton/lib/create_worksets.py
@@ -10,6 +10,7 @@ from Autodesk.Revit.DB import (
 )
 
 import ExcelUtils
+from HtmlUtils import HtmlUtils
 
 from pyrevit import forms, script
 
@@ -18,6 +19,8 @@ output.close_others()
 
 uidoc = __revit__.ActiveUIDocument  # type: ignore
 doc = uidoc.Document
+
+html_utils = HtmlUtils()
 
 
 def get_workset_names(path, discipline):
@@ -39,30 +42,53 @@ def get_workset_names(path, discipline):
     return [x for x in workset_names if x is not None]
 
 
-def get_rename_existing_dict(user_workset_names):
-    rename_dict = {
-        "Shared Levels and Grids": None,
-        "Shared Views, Levels, Grids": None,
-        "Shared Levels & Grids": None,
-        "Workset1": None,
-        "Workset 1": None,
-    }
+def get_rename_dict_list(user_workset_names):
+    rename_dict_list = [
+        {
+            "options": [
+                "Shared Levels and Grids".lower(),
+                "Shared_Levels_and_Grids".lower(),
+                "Shared_Levels_Grids".lower(),
+                "Shared Views, Levels, Grids".lower(),
+                "Shared Levels & Grids".lower(),
+            ],
+            "rename_to": None,
+        },
+        {
+            "options": ["Workset1".lower(), "Workset 1".lower(), "Workset_1".lower()],
+            "rename_to": None,
+        },
+    ]
+
     new_user_workset_names = []
-    for workset_name in user_workset_names:
-        for key in rename_dict:
-            if key in workset_name:
-                rename_dict[key] = workset_name
+
+    def get_rename_to(user_workset_name, item):
+        user_workset_name_lower = user_workset_name.lower()
+        options = item["options"]
+
+        for option in options:
+            if option in user_workset_name_lower:
+                return user_workset_name
+
+        return None
+
+    for user_workset_name in user_workset_names:
+        for item in rename_dict_list:
+            rename_to = get_rename_to(user_workset_name, item)
+            if rename_to:
+                item["rename_to"] = user_workset_name
                 break
         else:
-            new_user_workset_names.append(workset_name)
-    return rename_dict, new_user_workset_names
+            new_user_workset_names.append(user_workset_name)
+
+    return rename_dict_list, new_user_workset_names
 
 
 def is_workset_in_use(workset_name):
     return not WorksetTable.IsWorksetNameUnique(doc, workset_name)
 
 
-def rename_existing_workset(rename_dict):
+def rename_existing_workset(rename_dict_list):
     t = Transaction(doc, "BPM | Rename Worksets")
     t.Start()
     all_worksets = (
@@ -70,23 +96,29 @@ def rename_existing_workset(rename_dict):
     )
     for workset in all_worksets:
         workset_old_name = workset.Name
-        if workset_old_name in rename_dict:
-            workset_new_name = rename_dict[workset_old_name]
-            if workset_new_name is None:
-                continue
-            if is_workset_in_use(workset_new_name):
-                output.print_html(
-                    '<div style="color:red" >Workset {} is in use. The name of the workset {} is not changed.</div>'.format(
-                        workset_new_name, workset_old_name
+        for item in rename_dict_list:
+            if workset_old_name.lower() in item["options"]:
+                workset_new_name = item["rename_to"]
+                if workset_new_name is None:
+                    html_utils.add_html(
+                        '<div style="color:red" >Workset {} is in "None". The name of the workset {} is not changed.</div>'.format(
+                            workset_old_name, workset_old_name
+                        )
+                    )
+                    continue
+                if is_workset_in_use(workset_new_name):
+                    html_utils.add_html(
+                        '<div style="color:red" >Workset {} is in use. The name of the workset {} is not changed.</div>'.format(
+                            workset_new_name, workset_old_name
+                        )
+                    )
+                    continue
+                WorksetTable.RenameWorkset(doc, workset.Id, workset_new_name)
+                html_utils.add_html(
+                    '<div style="color:green" >Workset {} renamed to {}</div>'.format(
+                        workset_old_name, workset_new_name
                     )
                 )
-                continue
-            WorksetTable.RenameWorkset(doc, workset.Id, workset_new_name)
-            output.print_html(
-                '<div style="color:green" >Workset {} renamed to {}</div>'.format(
-                    workset_old_name, workset_new_name
-                )
-            )
     t.Commit()
 
 
@@ -95,25 +127,23 @@ def main(excel_path, discipline):
         forms.alert("Document is not workshared")
         return
 
-    output.print_html("<h1>Workset From Excel</h1>")
+    html_utils.add_html("<h1>Workset From Excel</h1>")
     t_group = TransactionGroup(doc, "BPM | Set BPM Worksets")
     t_group.Start()
 
-    output.print_html(
+    html_utils.add_html(
         "<h3>Excel Path: {} | Discipline: {}</h3>".format(excel_path, discipline)
     )
 
     user_workset_names = get_workset_names(excel_path, discipline)
 
-    rename_existing_dict, user_workset_names = get_rename_existing_dict(
-        user_workset_names
-    )
-    rename_existing_workset(rename_existing_dict)
+    rename_dict_list, user_workset_names = get_rename_dict_list(user_workset_names)
+    rename_existing_workset(rename_dict_list)
 
     workset_names = []
     for user_workset_name in user_workset_names:
         if is_workset_in_use(user_workset_name):
-            output.print_html(
+            html_utils.add_html(
                 '<div style="color:red">Workset {} is in use</div>'.format(
                     user_workset_name
                 )
@@ -121,17 +151,18 @@ def main(excel_path, discipline):
             continue
         workset_names.append(user_workset_name)
     if len(workset_names) == 0:
-        output.print_html('<div style="color:red" >No worksets to create</div>')
+        html_utils.add_html('<div style="color:red" >No worksets to create</div>')
         t_group.Assimilate()
         return
-    output.print_html("<h4>{} worksets to create:</h4>".format(len(workset_names)))
+    html_utils.add_html("<h4>{} worksets to create:</h4>".format(len(workset_names)))
     t = Transaction(doc, "BPM | Create Worksets")
     t.Start()
     for workset_name in workset_names:
         Workset.Create(doc, workset_name)
-        output.print_html(
+        html_utils.add_html(
             '<div style="color:green" >Workset {} created</div>'.format(workset_name)
         )
     t.Commit()
     t_group.Assimilate()
-    output.print_html('<h4 style="margin-top:8px; color:blue">Done</h4>')
+    html_utils.add_html('<h4 style="margin-top:8px; color:blue">Done</h4>')
+    output.print_html(html_utils.get_html())
