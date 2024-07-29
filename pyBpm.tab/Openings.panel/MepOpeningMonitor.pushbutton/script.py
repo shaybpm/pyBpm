9f90ccff5b2be6c31a9ad316e846351e1f807766
eyalBPM <eyal@bpm.org.il>
2024-07-29 10:41:34 +0300

    init

---
+++ pyBpm.tab/Openings.panel/MepOpeningMonitor.pushbutton/script.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+""" Monitor MEP penetrating concrete without openings modeling """
+__title__ = "MEP Opening\nMonitor"
+__author__ = "BPM"
+
+# -------------------------------
+# ------------IMPORTS------------
+# -------------------------------
+
+# from Autodesk.Revit.DB import ...
+# from Autodesk.Revit.UI import ...
+
+# from pyrevit import forms, script
+
+# import RevitUtils  # type: ignore
+# import HttpRequest  # type: ignore
+# import PyUtils  # type: ignore
+
+# -------------------------------
+# -------------MAIN--------------
+# -------------------------------
+
+uidoc = __revit__.ActiveUIDocument  # type: ignore
+doc = uidoc.Document
+# selection = [doc.GetElement(x) for x in uidoc.Selection.GetElementIds()]
+
+# --------------------------------
+# -------------SCRIPT-------------
+# --------------------------------
+
+
+def run():
+    pass
+
+
+run()
