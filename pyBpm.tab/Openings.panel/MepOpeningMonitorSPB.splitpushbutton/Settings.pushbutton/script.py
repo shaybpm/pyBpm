21a4f034e64934df0342cd46b26fea4cd3fea322
eyalBPM <eyal@bpm.org.il>
2024-08-21 12:58:33 +0300

    add setting pushbutton

---
+++ pyBpm.tab/Openings.panel/MepOpeningMonitorSPB.splitpushbutton/Settings.pushbutton/script.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+""" Settings for MepOpeningMonitor """
+__title__ = "Settings"
+__author__ = "BPM"
+
+# -------------------------------
+# ------------IMPORTS------------
+# -------------------------------
+
+# from Autodesk.Revit.DB import ...
+# from Autodesk.Revit.UI import ...
+
+# from pyrevit import forms, script
+
+# import RevitUtils
+# import HttpRequest
+# import PyUtils
+
+# -------------------------------
+# -------------MAIN--------------
+# -------------------------------
+
+uidoc = __revit__.ActiveUIDocument  # type: ignore
+doc = uidoc.Document
+
+# --------------------------------
+# -------------SCRIPT-------------
+# --------------------------------
+
+
+def run():
+    pass
+
+
+run()
