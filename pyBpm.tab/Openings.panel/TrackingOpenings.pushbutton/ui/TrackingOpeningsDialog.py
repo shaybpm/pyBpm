3a01f446b0abba747081088986c8cf52ea182b4c
eyalBPM <eyal@bpm.org.il>
2024-11-28 10:39:07 +0200

    Update currentRealLevel assignment logic to default to "None" when no level is found in TrackingOpeningsDialog.py

--- pyBpm.tab/Openings.panel/TrackingOpenings.pushbutton/ui/TrackingOpeningsDialog.py
+++ pyBpm.tab/Openings.panel/TrackingOpenings.pushbutton/ui/TrackingOpeningsDialog.py
@@ -203,9 +203,9 @@ class TrackingOpeningsDialog(Windows.Window):
                     (bbox.Min.Z + bbox.Max.Z) / 2,
                 )
                 currentLevel = get_level_by_point(center_point, self.doc, True)
-                opening["currentRealLevel"] = currentLevel.Name if currentLevel else ""
+                opening["currentRealLevel"] = currentLevel.Name if currentLevel else "None"
             else:
-                opening["currentRealLevel"] = ""
+                opening["currentRealLevel"] = "None"
 
         self._openings = value
         self.display_openings = value
