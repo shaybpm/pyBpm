6b1c2a2c27fc23192ad23ec6b875ccbfae787a02
eyalBPM <eyal@bpm.org.il>
2025-10-23 14:36:43 +0300

    Add Area Data Formatter script to process area parameters in Revit

---
+++ pyBpm.tab/BPM.panel/Custom.pushbutton/lib/AreaDataFormatter/AreaDataFormatter.py
@@ -0,0 +1,78 @@
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, Transaction
+from pyrevit import forms
+from ProgressBar import ProgressBar, UserCanceledException
+
+
+def area_data_formatter(uidoc):
+    doc = uidoc.Document
+    areas = (
+        FilteredElementCollector(doc)
+        .OfCategory(BuiltInCategory.OST_Areas)
+        .WhereElementIsNotElementType()
+        .ToElements()
+    )
+    if not areas:
+        forms.alert("No areas found in the document.", title="Area Data Formatter")
+        return
+
+    target_parameter_name = "Apartment Code"
+
+    source1_parameter_name = "Building Number"
+    source2_parameter_name = "Level"
+    source3_parameter_name = "Apartment"
+    source3_parameter_name = "NumberRooms"
+
+    def ex_func(progress_bar):  # type: (ProgressBar) -> None
+        progress_bar.pre_set_main_status("{}/{}".format(0, float(len(areas))))
+        t = Transaction(doc, "pyBpm | Area Data Formatter")
+        t.Start()
+        try:
+            for i, area in enumerate(areas):
+                param_target = area.LookupParameter(target_parameter_name)
+                param_source1 = area.LookupParameter(source1_parameter_name)
+                param_source2 = area.LookupParameter(source2_parameter_name)
+                param_source3 = area.LookupParameter(source3_parameter_name)
+                param_source4 = area.LookupParameter(source3_parameter_name)
+
+                # if some parameter is missing, rollback and alert
+                if not all(
+                    [
+                        param_target,
+                        param_source1,
+                        param_source2,
+                        param_source3,
+                        param_source4,
+                    ]
+                ):
+                    t.RollBack()
+                    forms.alert(
+                        "One or more parameters are missing",
+                        title="Area Data Formatter",
+                    )
+                    return
+
+                value1 = param_source1.AsString() or "?"
+                value2 = param_source2.AsString() or "?"
+                value3 = param_source3.AsString() or "?"
+                value4 = param_source4.AsString() or "?"
+
+                formatted_value = "{}-{}-{}-{}".format(value1, value2, value3, value4)
+                param_target.Set(formatted_value)
+
+                main_percent = (i + 1) * (100 / float(len(areas)))
+                main_text = "{}/{}".format(i + 1, len(areas))
+                progress_bar.update_main_status(main_percent, main_text)
+        except UserCanceledException:
+            if t.HasStarted() and not t.HasEnded():
+                t.RollBack()
+            return
+        except Exception as ex:
+            forms.alert(str(ex), title="Error")
+        finally:
+            progress_bar.Close()
+            
+        t.Commit()
+
+    ProgressBar.exec_with_progressbar(
+        ex_func, title="Area Data Formatter", cancelable=True
+    )
--- pyBpm.tab/BPM.panel/Custom.pushbutton/lib/Custom.py
+++ pyBpm.tab/BPM.panel/Custom.pushbutton/lib/Custom.py
@@ -4,21 +4,20 @@ import sys, os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "BondsViewer"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "GetBpmAcViews"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "AreaDataFormatter"))
 from BondsViewer import bonds_viewer  # type: ignore
 from GetBpmAcViews import get_bpm_ac_view_templates  # type: ignore
+from AreaDataFormatter import area_data_formatter  # type: ignore
 
 
 def common_scripts():
     """Return a list of common scripts."""
-    return [
-        "Bonds Viewer",
-        "Get BPM AC View Templates"
-    ]
+    return ["Bonds Viewer", "Get BPM AC View Templates"]
 
 
 def custom_scripts(doc):
     """Return a list of scripts by project."""
-    return []
+    return ["Area Data Formatter"]
 
 
 def run_by_name(name, uidoc):
@@ -27,5 +26,7 @@ def run_by_name(name, uidoc):
         bonds_viewer(uidoc)
     elif name == "Get BPM AC View Templates":
         get_bpm_ac_view_templates(uidoc)
+    elif name == "Area Data Formatter":
+        area_data_formatter(uidoc)
     else:
         raise ValueError("Script '{}' not found.".format(name))
